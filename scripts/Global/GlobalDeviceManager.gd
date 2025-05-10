49a194ab6e61680a2cac2f3b045239f485594b6e
Joshua Petherick <peth@live.co.uk>
2025-05-10 20:23:48 +0100

    Improved GlobalDeviceManager

--- scripts/Global/GlobalDeviceManager.gd
+++ scripts/Global/GlobalDeviceManager.gd
@@ -7,18 +7,30 @@ var _is_listening: bool = false
 
 #region Events
 
+func _ready() -> void:
+	# Add default device for Player 1
+	_player_dict[1] = 0
+	
+	# Setup Signal
+	Input.joy_connection_changed.connect(on_joy_connection_changed)
+
 func _input(event: InputEvent) -> void:
 	# Checks
 	if (!_is_listening):
 		return;
 	
-	if (Input.is_joy_known(event.device) != true):
-		return;
+	if (Input.is_action_just_pressed("player_action_1") != true
+		&& Input.is_action_just_pressed("player_action_2") != true
+		&& Input.is_action_just_pressed("player_action_3") != true
+		&& Input.is_action_just_pressed("player_action_4") != true
+		&& Input.is_action_just_pressed("player_action_5") != true
+		&& Input.is_action_just_pressed("player_action_6") != true
+		&& Input.is_action_just_pressed("player_action_7") != true
+		&& Input.is_action_just_pressed("player_action_8") != true):
+		return
 	
 	# Assign Device to Player
 	_player_dict[_player_index] = event.device
-	
-	var test = Input.get_joy_info(event.device)
 	
 	# Emit Signal
 	GlobalSignals.device_connected.emit(_player_index)
@@ -26,6 +38,17 @@ func _input(event: InputEvent) -> void:
 	# Stop Listening
 	_player_index = 0
 	_is_listening = false
+
+func on_joy_connection_changed(device: int, connected: bool):
+	# Check
+	if (connected == true):
+		if (_player_dict.has(1) != true):
+			_player_dict[1] = device
+		elif (_player_dict.has(2) != true):
+			_player_dict[2] = device
+	else:
+		if (_player_dict.values().has(device)):
+			_player_dict.erase(get_player_from_device(device))
 
 #endregion
 
