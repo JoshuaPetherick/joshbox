f383cee35b380cd9261ae7522d99cb9a592467a1
Joshua Petherick <peth@live.co.uk>
2025-05-23 20:17:51 +0100

    Modified player script to pickup device input

--- scripts/Games/3DPlatformer/player.gd
+++ scripts/Games/3DPlatformer/player.gd
@@ -1,20 +1,22 @@
 extends CharacterBody3D
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const SPEED = 3.0
+const JUMP_VELOCITY = 5.0
+
+@export 
+var player_id: int = 1
+
+var up_strength: float 
+var down_strength: float 
+var left_strength: float 
+var right_strength: float 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("player_action_8") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("player_action_9", "player_action_11", "player_action_10", "player_action_12")
+	
+	var input_dir = Vector2(-left_strength + right_strength, -up_strength + down_strength)
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -22,5 +24,42 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	
+	# Move Player Character
 	move_and_slide()
+
+func _input(event: InputEvent) -> void:
+	# Setup
+	var player = GlobalDeviceManager.get_player_from_event(event)
+	
+	# Checks
+	if (player == null):
+		return
+	
+	if (player != player_id):
+		return
+	
+	# Get Movement Actions
+	if (event.is_action_pressed("player_action_10")):
+		up_strength = event.get_action_strength("player_action_10")
+	elif (event.is_action_released("player_action_10")):
+		up_strength = 0
+		
+	if (event.is_action_pressed("player_action_12")):
+		down_strength = event.get_action_strength("player_action_12")
+	elif (event.is_action_released("player_action_12")):
+		down_strength = 0
+		
+	if (event.is_action_pressed("player_action_9")):
+		left_strength = event.get_action_strength("player_action_9")
+	elif (event.is_action_released("player_action_9")):
+		left_strength = 0
+		
+	if (event.is_action_pressed("player_action_11")):
+		right_strength = event.get_action_strength("player_action_11")
+	elif (event.is_action_released("player_action_11")):
+		right_strength = 0
+	
+	# Handle jump.
+	if event.is_action("player_action_8") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
