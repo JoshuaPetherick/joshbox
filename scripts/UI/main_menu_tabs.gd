4d0c84d21cfc3bd1754e9733c1f21a24fdecafa4
Joshua Petherick <peth@live.co.uk>
2025-06-04 21:09:23 +0100

    Setup UI focus logic for Controllers

--- scripts/UI/main_menu_tabs.gd
+++ scripts/UI/main_menu_tabs.gd
@@ -1,5 +1,12 @@
 extends TabContainer
 
+@export var tab_1_default_focus: Button
+@export var tab_2_default_focus: Button
+@export var tab_3_default_focus: Button
+@export var tab_4_default_focus: Button
+@export var tab_5_default_focus: Button
+@export var tab_6_default_focus: Button
+
 func _ready() -> void:
 	# Select based on Game Type
 	match GlobalGameProperties.game_type:
@@ -11,6 +18,12 @@ func _ready() -> void:
 			current_tab = 1
 		_:
 			current_tab = 0
+	
+	# Apply Button Focus
+	_on_tab_change(current_tab)
+	
+	# Setup Signal
+	tab_changed.connect(_on_tab_change)
 
 func _on_game_selection_pressed() -> void:
 	current_tab = 1
@@ -26,3 +39,12 @@ func _on_settings_pressed() -> void:
 
 func _on_back_pressed() -> void:
 	current_tab = 0
+
+func _on_tab_change(tab: int) -> void:
+	match tab:
+		0: tab_1_default_focus.grab_focus()
+		1: tab_2_default_focus.grab_focus()
+		2: tab_3_default_focus.grab_focus()
+		3: tab_4_default_focus.grab_focus()
+		4: tab_5_default_focus.grab_focus()
+		5: tab_6_default_focus.grab_focus()
